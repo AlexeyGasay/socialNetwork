8634a17601319847b74685af2abe21f42e219290
Alexey Indyuchenko <46484506+AlexeyGasay@users.noreply.github.com>
2022-07-12 11:16:01 +0300

    Update database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,40 +1,9 @@
-create TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    surname VARCHAR(255)
-);
+CREATE DATABASE tumblr;
 
-create TABLE posts (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
-    content VARCHAR(255),
-    user_id INTEGER,
-    FOREIGN KEY (user_id) REFERENCES person(id)
-);
-
-
-
-
-
-
-
-
-
----------------------   ТАБМЛЕР
-
--- ЮЗЕРЫ
-create TABLE user_list (user_id BIGSERIAL PRIMARY KEY, username VARCHAR(15), password VARCHAR(100), user_avatar TEXT DEFAULT null, CONSTRAINT username_unique UNIQUE (username));
+CREATE TABLE user_list (user_id BIGSERIAL PRIMARY KEY, username VARCHAR(15), password VARCHAR(100), user_avatar TEXT DEFAULT null, CONSTRAINT username_unique UNIQUE (username));
 
 INSERT INTO user_list (username, password) VALUES ('qwe', 'qwe');
 
--- CREATE TABLE avatar_images (
---     user_id BIGSERIAL PRIMARY KEY,
---     file_path TEXT,
---     FOREIGN KEY (user_id) REFERENCES user_list(user_id) ON DELETE CASCADE
--- );      
-
-
--- ДРУЗЬЯ
 CREATE TABLE friends (
     id BIGSERIAL PRIMARY KEY,
     friend_one INTEGER NOT NULL,
@@ -44,38 +13,7 @@ CREATE TABLE friends (
     FOREIGN KEY (friend_two) REFERENCES user_list(user_id) ON DELETE CASCADE
     );
 
-INSERT INTO friends (friend_one, friend_two) VALUES (1, 4);
 
--- UPDATE friends 
--- SET status = 1
--- WHERE
--- (friend_one=3 OR friend_two=3)
--- AND
--- (friend_one=1 OR friend_two=1);
-
-UPDATE friends 
-SET status = 1
-WHERE
-friend_one=1 AND friend_two=6;
-
-
-
-SELECT * FROM friends WHERE friend_one = 1 AND status = 1 OR friend_two = 1 AND status = 1; 
-
-WITH friend_list AS (
-    SELECT * FROM friends WHERE
-    friends.friend_one = 1 AND status = 1
-    OR
-    friends.friend_two = 1 AND status = 1
-)
-SELECT * FROM friend_list;
-
-
-
-
-
-
--- ПОСТЫ
 CREATE TABLE post_list (
     post_id BIGSERIAL PRIMARY KEY,
     author BIGINT NOT NULL,
@@ -87,18 +25,6 @@ CREATE TABLE post_list (
     FOREIGN KEY (author_name) REFERENCES user_list(username)
 );
 
-insert into post_list
-(author, text_content, title_text)
-values
-(1, 'qwe', 'qwe');
-
-
-ALTER TABLE post_list ALTER COLUMN author SET NOT NULL;
-
-ALTER TABLE table_name RENAME TO new_table_name;
-
-
--- ФАЙЛЫ ПОСТОВ
 
 CREATE TABLE file_to_post (
     post_id BIGSERIAL,
@@ -107,7 +33,6 @@ CREATE TABLE file_to_post (
 );
 
 
--- КОММЕНТАРИИ
 CREATE TABLE post_comments (
     id BIGSERIAL PRIMARY KEY,
     post_id BIGSERIAL,
@@ -119,11 +44,6 @@ CREATE TABLE post_comments (
     FOREIGN KEY (author_username) REFERENCES user_list(username)
 );
 
-INSERT INTO post_comments (post_id, author_id, author_username, comment_text) VALUES (50, 2, 'qwe', 'COMMET');
-
-
--- ЛАЙКИ
-
 CREATE TABLE like_list (
     user_id BIGINT,
     post_id BIGINT,
@@ -131,18 +51,6 @@ CREATE TABLE like_list (
     FOREIGN KEY (post_id) REFERENCES post_list(post_id) ON DELETE CASCADE
 );
 
-
--- FULLTEXT SEARCH
-
-CREATE TABLE news (
-    id BIGSERIAL,
-    title TEXT,
-    content TEXT
-);
-select * from news where to_tsvector(title) || to_tsvector(content) @@ plainto_tsquery('фруктового');
-
-
--- MESSAGES
 
 CREATE TABLE message_list (
     id BIGSERIAL PRIMARY KEY,
@@ -153,5 +61,3 @@ CREATE TABLE message_list (
     FOREIGN KEY (sender) REFERENCES user_list(user_id),
     FOREIGN KEY (recipient) REFERENCES user_list(user_id)
 );
-
-INSERT INTO message_list (sender, recipient, message_text, create_time) values (2, 4, 'opoposapod', '12.11');
